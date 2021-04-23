c0fc3b9657dfec3dc7bc5af1d91fa83316fba46a
sholvoir <shaw.he@hotmail.com>
2021-04-22 20:53:32 -0400

    getArgs

---
+++ args/mod.ts
@@ -0,0 +1,9 @@
+export function getArgs() {
+    const options = new Map<string, string>();
+    const args = new Array<string>();
+    for (const arg of Deno.args) if (arg.startsWith('-')) {
+        const [option, value] = arg.split('=');
+        options.set(option, value || '');
+    } else args.push(arg);
+    return [options, args];
+}
