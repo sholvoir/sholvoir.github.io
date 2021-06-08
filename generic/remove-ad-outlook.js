b7b24d6cbed9bf5108f3118e453ec5cda9e4eb0d
sholvoir <shaw.he@hotmail.com>
2021-06-08 09:58:10 -0400

    remove export

--- generic/remove-ad-outlook.js
+++ generic/remove-ad-outlook.js
@@ -1,4 +1,4 @@
-export function r(){
-    let x = document.querySelector('div._1xCBLVKl1qp6BB2jT7GN5b');
-    x.lastChild.remove();
-}
+(() => {
+let x = document.querySelector('div._1xCBLVKl1qp6BB2jT7GN5b');
+x.lastChild.remove();
+})();
