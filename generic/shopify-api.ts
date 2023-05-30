800befe158e292871e7f4429f0fc9fa1f008d0ac
sholvoir <sovar.he@gmail.com>
2023-05-30 19:36:13 -0400

    May30

--- generic/shopify-api.ts
+++ generic/shopify-api.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import { HTTPMethod } from 'https://www.micinfotech.com/generic/http-method.ts';
+import { HTTPMethod } from 'https://sholvoir.github.io/generic/http-method.ts';
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
